ed10ea05136a9c607591363c0f152590cfc0dc2d
Tobshub <tobanigeorge@gmail.com>
2023-04-13 18:58:56 +0100

    include token in trpc requests

---
+++ client/src/utils/token.ts
@@ -0,0 +1,18 @@
+const tokenName = "remote-img-token";
+const clientToken = {
+  set: (value: string) => {
+    localStorage.setItem(tokenName, value);
+    return value;
+  },
+  get: () => {
+    const token = localStorage.getItem(tokenName);
+    return token;
+  },
+  remove: () => {
+    const token = localStorage.getItem(tokenName);
+    localStorage.removeItem(tokenName);
+    return token;
+  }
+};
+
+export default clientToken;
