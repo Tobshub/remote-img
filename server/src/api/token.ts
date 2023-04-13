cc624bba4dfabf239d9bfeaf0424b0a08a61b57f
Tobshub <tobanigeorge@gmail.com>
2023-04-13 09:20:04 +0100

    gen & validate jwt tokens

---
+++ server/src/api/token.ts
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import Log from "../config/logger";
+import { Err, Ok } from "../helpers/result";
+import { env } from "../index";
+
+const { jwtSecret } = env;
+const appToken = {
+  generate: (seed: string) => {
+    try {
+      if (!jwtSecret) throw new Error("JWT secret missing");
+      const payload = { data: seed, iat: Date.now() / 1000 };
+      const token = jwt.sign(payload, jwtSecret, { expiresIn: "30 days" });
+      return Ok(token);
+    } catch (err) {
+      Log.error(err, "Failed to generate token");
+      return Err("Failed to generate token");
+    }
+  },
+  validate: (token: string) => {
+    try {
+      if (!jwtSecret) throw new Error("JWT secret missing");
+      const isValid = jwt.verify(token, jwtSecret) as { data: string };
+      return Ok(isValid.data);
+    } catch (err) {
+      return Err("Token validation failed");
+    }
+  },
+};
+
+export default appToken;
