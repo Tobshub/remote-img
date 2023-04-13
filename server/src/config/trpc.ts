dee8bf5f34713e5907829968bc944e531ab53c0a
Tobshub <tobanigeorge@gmail.com>
2023-04-13 09:20:36 +0100

    authed procedure

--- server/src/config/trpc.ts
+++ server/src/config/trpc.ts
@@ -1,10 +1,10 @@
 import { TRPCError, inferAsyncReturnType, initTRPC } from "@trpc/server";
 import * as trpcExpress from "@trpc/server/adapters/express";
+import appToken from "../api/token";
 
-export const createContext = ({
-  req,
-}: trpcExpress.CreateExpressContextOptions) => ({
+export const createContext = ({ req }: trpcExpress.CreateExpressContextOptions) => ({
   auth: req.headers.authorization,
+  isAuth: false,
 });
 
 export type Context = inferAsyncReturnType<typeof createContext>;
@@ -13,3 +13,16 @@ const trpc = initTRPC.context<Context>().create();
 export const tRouter = trpc.router;
 export const tProcedure = trpc.procedure;
 export const tError = TRPCError;
+
+const AuthMiddleware = trpc.middleware(async ({ ctx, next }) => {
+  if (!ctx.auth) {
+    throw new tError({ code: "UNAUTHORIZED", message: "Please Login or Sign up." });
+  }
+  const isValid = appToken.validate(ctx.auth);
+  if (!isValid.ok) {
+    throw new tError({ code: "UNAUTHORIZED", message: "Please Login again." });
+  }
+  return next({ ctx: { auth: ctx.auth, isAuth: true } });
+});
+
+export const authProcedure = tProcedure.use(AuthMiddleware);
