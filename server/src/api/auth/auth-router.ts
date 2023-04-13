e419b5de1ccb577056e25722adbf846705f9cfc0
Tobshub <tobanigeorge@gmail.com>
2023-04-13 18:35:19 +0100

    auth

---
+++ server/src/api/auth/auth-router.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+import { tError, tProcedure, tRouter } from "../../config/trpc";
+import { env } from "../../index";
+import appToken from "../token";
+
+const authRouter = tRouter({
+  login: tProcedure
+    .input(z.object({ email: z.string().email(), password: z.string() }))
+    .mutation(async ({ input }) => {
+      const valid = input.email === env.email && input.password === env.password;
+
+      if (!valid) {
+        throw new tError({ code: "BAD_REQUEST", message: "Email or Password is wrong" });
+      }
+
+      const token = appToken.generate(input.email);
+      if (token.ok) {
+        return token;
+      }
+
+      throw new tError({ code: "INTERNAL_SERVER_ERROR", message: token.message });
+    }),
+});
+
+export default authRouter;
--- server/src/api/router.ts
+++ server/src/api/router.ts
@@ -1,4 +1,5 @@
 import { tRouter } from "../config/trpc";
+import authRouter from "./auth/auth-router";
 import retrieveRouter from "./retrieve/retrieve-router";
 import uploadRouter from "./upload/upload-router";
 
@@ -7,6 +8,8 @@ export const appRouter = tRouter({
   upload: uploadRouter,
   /** Router that handles image retrieval */
   retrieve: retrieveRouter,
+  /** Router that handles user auth */
+  auth: authRouter,
 });
 
 export const appRouterCaller = appRouter.createCaller({ auth: undefined, isAuth: true });
