299def92fdfd8c48d7794242b3304dcf3fbc1c3c
Tobshub <tobanigeorge@gmail.com>
2023-04-14 22:05:07 +0100

    shortened generated url

--- server/src/api/upload/controller/url.ts
+++ server/src/api/upload/controller/url.ts
@@ -1,5 +1,5 @@
 export default function createUrl(options?: { isPerm?: boolean }) {
-  const prefix = `${options?.isPerm ? "p_" : "t_"}tobs`;
+  const prefix = options?.isPerm ? "p_" : "t_";
   const rand = (Math.random() + 1).toString(36).substring(2);
   const url = `${prefix}_${rand}`;
   return url;
