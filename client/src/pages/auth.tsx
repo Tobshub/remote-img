9769ca6e5c027399fd59137a9ce51ad67f866910
Tobshub <tobanigeorge@gmail.com>
2023-04-14 07:06:51 +0100

    home button

--- client/src/pages/auth.tsx
+++ client/src/pages/auth.tsx
@@ -1,7 +1,7 @@
 import clientToken from "@/utils/token";
 import { trpc } from "@/utils/trpc";
 import { FormEvent, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function AuthPage() {
   const [user, setUser] = useState({ email: "", password: "" });
@@ -31,47 +31,43 @@ export default function AuthPage() {
     await loginMut.mutateAsync({ email: user.email, password: user.password }).catch((_) => null);
   };
   return (
-    <div
-      className={`
-        page login-page 
-        d-flex 
-        flex-column 
-        justify-content-center 
-        align-items-center
-        fs-6
-      `}
-    >
-      <h1>Login</h1>
-      {mutationRes ? <p>{mutationRes}</p> : null}
-      <form onSubmit={handleSubmit}>
-        <label className="d-block mb-3">
-          <span>Email: </span>
-          <input
-            className="form-control fs-5"
-            type="email"
-            placeholder="example@domain.com"
-            onChange={(e) => handleChange("email", e.target.value)}
-          />
-        </label>
-        <label className="d-block mb-3">
-          <span>Password: </span>
-          <input
-            className="form-control fs-5"
-            type="password"
-            placeholder="xxxxxx"
-            onChange={(e) => handleChange("password", e.target.value)}
-          />
-        </label>
-        <div>
-          <button
-            type="submit"
-            className="btn btn-primary"
-            disabled={loginMut.isLoading || !user.email || !user.password}
-          >
-            SUBMIT
-          </button>
-        </div>
-      </form>
+    <div className="page">
+      <Link to="/" className="btn btn-primary rounded-0">
+        HOME
+      </Link>
+      <div className="d-flex flex-column justify-content-center align-items-center fs-5">
+        <h1 className="text-center">Login</h1>
+        {mutationRes ? <p>{mutationRes}</p> : null}
+        <form onSubmit={handleSubmit}>
+          <label className="d-block mb-3">
+            <span>Email: </span>
+            <input
+              className="form-control fs-5"
+              type="email"
+              placeholder="example@domain.com"
+              onChange={(e) => handleChange("email", e.target.value)}
+            />
+          </label>
+          <label className="d-block mb-3">
+            <span>Password: </span>
+            <input
+              className="form-control fs-5"
+              type="password"
+              placeholder="xxxxxx"
+              onChange={(e) => handleChange("password", e.target.value)}
+            />
+          </label>
+          <div>
+            <button
+              type="submit"
+              className="btn btn-primary"
+              disabled={loginMut.isLoading || !user.email || !user.password}
+            >
+              SUBMIT
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
