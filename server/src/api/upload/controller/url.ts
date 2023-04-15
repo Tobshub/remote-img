7257dd820378740ebb9eb0321105898ee03bed46
Tobshub <tobanigeorge@gmail.com>
2023-04-15 02:31:45 +0100

    fixed generated url

--- server/src/api/upload/controller/url.ts
+++ server/src/api/upload/controller/url.ts
@@ -1,5 +1,5 @@
 export default function createUrl(options?: { isPerm?: boolean }) {
-  const prefix = options?.isPerm ? "p_" : "t_";
+  const prefix = options?.isPerm ? "p" : "t";
   const rand = (Math.random() + 1).toString(36).substring(2);
   const url = `${prefix}_${rand}`;
   return url;
