5780193b144b92d27b37760119f073e0fd97b720
Tobshub <tobanigeorge@gmail.com>
2023-04-16 20:53:18 +0100

    toggle between temp and perm upload

--- client/src/pages/upload.tsx
+++ client/src/pages/upload.tsx
@@ -2,12 +2,14 @@ import { FormEvent, useRef, useState } from "react";
 import { trpc } from "@/utils/trpc";
 import { serverUrl } from "@/data/url";
 import { Link } from "react-router-dom";
+import tempUpload from "@server/api/upload/controller/temp";
 
 export default function UploadPage() {
   const [responseMsg, setResponseMsg] = useState("");
   const [uploads, setUploads] = useState<string[]>([]);
+  const [uploadType, setUploadType] = useState<"perm" | "temp">("temp");
 
-  const uploadMut = trpc.upload.permUpload.useMutation({
+  const permUploadMut = trpc.upload.permUpload.useMutation({
     onSuccess(data) {
       if (data.ok) {
         setResponseMsg(`Success: Image url is ${serverUrl}/img/${data.value}`);
@@ -18,8 +20,26 @@ export default function UploadPage() {
       setResponseMsg(`Error: ${err.message}`);
     },
   });
+
+  const tempUploadMut = trpc.upload.tempUpload.useMutation({
+    onSuccess(data) {
+      if (data.ok) {
+        setResponseMsg(`Success: Image url is ${serverUrl}/img/${data.value}`);
+        setUploads((state) => [...state, data.value]);
+      }
+    },
+    onError(err) {
+      setResponseMsg(`Error: ${err.message}`);
+    },
+  });
+
   const uploadFileData = async (data: string, type: string) => {
-    await uploadMut.mutateAsync({ data, type }).catch((_) => null);
+    if (uploadType === "perm") {
+      await permUploadMut.mutateAsync({ data, type }).catch((_) => null);
+      return;
+    }
+    await tempUploadMut.mutateAsync({ data, type }).catch((_) => null);
+    return;
   };
 
   // read data from image file and upload
@@ -54,15 +74,32 @@ export default function UploadPage() {
         </Link>
         <h2 className="text-center">Upload Image Files to Tobsmg server</h2>
       </div>
-      <div
-        className="mx-auto d-flex justify-content-between align-items-center px-4"
-      >
+      <div className="mx-auto d-flex justify-content-between align-items-center px-4">
         <form onSubmit={handleSubmit}>
           <label className="d-block mb-3">
             <span className="d-block mb-1">Select a file to upload</span>
             <input className="form-control" type="file" accept="image/*" ref={imageUploadRef} />
           </label>
-          <button type="submit" className="btn btn-success" disabled={uploadMut.isLoading}>
+          <div className="form-check form-switch mb-3">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              role="switch"
+              onChange={(e) => {
+                if (e.target.checked) {
+                  setUploadType("perm");
+                  return;
+                }
+                setUploadType("temp");
+              }}
+            />
+            <label>Permanent Upload</label>
+          </div>
+          <button
+            type="submit"
+            className="btn btn-success"
+            disabled={uploadType === "perm" ? permUploadMut.isLoading : tempUploadMut.isLoading}
+          >
             UPLOAD
           </button>
           {responseMsg.length ? <p>{responseMsg}</p> : null}
--- server/src/api/retrieve/controller/getAll.ts
+++ server/src/api/retrieve/controller/getAll.ts
@@ -4,14 +4,11 @@ import { Err, Ok } from "../../../helpers/result";
 
 export default async function getAllUrls(options?: { cursor?: string | null; sort?: string }) {
   try {
-    const refs = await usePrisma.permReference.findMany({
-      take: 30,
-      cursor: options?.cursor ? { url: options.cursor } : undefined,
-      select: { url: true },
-    });
+    const permRefs = await usePrisma.permReference.findMany({ select: { url: true } });
+    const tempRefs = await usePrisma.tempReference.findMany({ select: { url: true } });
 
     Log.info("Success getting image refs");
-    return Ok(refs);
+    return Ok(permRefs.concat(tempRefs));
   } catch (err) {
     Log.error(err, "Failed to get urls");
     return Err("Failed to get image urls");
