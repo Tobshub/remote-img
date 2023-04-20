d6a7813b4a83520c54b96edde5ea9f224c645c02
Tobshub <tobanigeorge@gmail.com>
2023-04-20 01:07:38 +0100

    preview image before delete

--- client/src/pages/images.tsx
+++ client/src/pages/images.tsx
@@ -1,6 +1,6 @@
 import { serverUrl } from "@/data/url";
 import { trpc } from "@/utils/trpc";
-import { useState } from "react";
+import { PropsWithRef, Ref, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function ImagesPage() {
@@ -10,21 +10,32 @@ export default function ImagesPage() {
   );
 
   const [imageFilter, setImageFilter] = useState("");
+  const deleteModalRef = useRef<HTMLDialogElement>(null);
 
   if (!imgRefs.data) {
     return <>Loading...</>;
   }
   return (
     <div className="page">
-      <Link to="/" className="btn btn-primary rounded-0">
-        HOME
-      </Link>
+      <div className="d-flex justify-content-start align-items-start gap-2 flex-wrap">
+        <Link to="/" className="btn btn-primary rounded-0">
+          HOME
+        </Link>
+        <button
+          className="btn btn-danger rounded-0"
+          title="open delete modal"
+          onClick={() => deleteModalRef.current?.showModal()}
+        >
+          DELETE IMAGE
+        </button>
+      </div>
+      <DeleteImageModal refProp={deleteModalRef} />
       <h1 className="text-center">All Images</h1>
       <form style={{ maxWidth: 500 }} className="mx-auto">
         <label>filter images:</label>
         <input
           placeholder="filter query"
-          className="form-control"
+          className="form-control mb-3"
           onChange={(e) => setImageFilter(e.target.value)}
         />
       </form>
@@ -35,10 +46,49 @@ export default function ImagesPage() {
         {imgRefs.data?.pages.map((page) =>
           page.value
             .filter((imageRef) => imageRef.name.includes(imageFilter))
-            .map((imageRef) => <DisplayImageComponent serverUrl={serverUrl} imageRef={imageRef} />)
+            .map((imageRef) => (
+              <DisplayImageComponent key={imageRef.url} serverUrl={serverUrl} imageRef={imageRef} />
+            ))
         ) ?? null}
       </ul>
     </div>
+  );
+}
+
+function DeleteImageModal(props: { refProp: Ref<HTMLDialogElement> }) {
+  const [url, setUrl] = useState("");
+  const imagePreviewRef = useRef<HTMLImageElement>(null);
+  const [showPreview, setShowPreview] = useState(false);
+
+  return (
+    <dialog ref={props.refProp} className="mx-auto my-auto">
+      <h3>Delete Image</h3>
+      <div className="d-flex">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setShowPreview(true);
+          }}
+        >
+          <label>URL:</label>
+          <input
+            placeholder={"x_xxxxxxxxxx"}
+            className="form-control"
+            onChange={(e) => {
+              setUrl(e.target.value);
+              setShowPreview(false);
+            }}
+          />
+          <button type="submit" className="btn btn-outline-danger" disabled={!url.length}>
+            search
+          </button>
+        </form>
+        {showPreview ? <img src={serverUrl + "/img/" + url} height={100} /> : null}
+      </div>
+      <form method="dialog">
+        <button className="btn btn-outline-secondary">CANCEL</button>
+      </form>
+    </dialog>
   );
 }
 
